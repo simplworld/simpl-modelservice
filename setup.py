9409dddfcfe43d2a1859cc1a0886768668c809b9
Jane Eisenstein <ejane@wharton.upenn.edu>
2019-07-31 11:03:20 -0400

    Bump version: 0.7.14 → 0.7.15

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-VERSION = '0.7.14'
+VERSION = '0.7.15'
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
     readme = f.read()
