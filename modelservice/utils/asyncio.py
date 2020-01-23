ff450219e48ba787eb2268e11979e000dd119b6d
Jane Eisenstein <ejane@wharton.upenn.edu>
2020-01-23 10:05:43 -0500

    Adds RuntimeError handling.

--- modelservice/utils/asyncio.py
+++ modelservice/utils/asyncio.py
@@ -26,8 +26,13 @@ def coro(f):
         f = asyncio.coroutine(f)
 
     def wrapper(*args, **kwargs):
-        loop = asyncio.get_event_loop()
+        try:
+            loop = asyncio.get_event_loop()
+        except RuntimeError:
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
         return loop.run_until_complete(f(*args, **kwargs))
+
     return update_wrapper(wrapper, f)
 
 
