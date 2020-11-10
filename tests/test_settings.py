952c6c79594d2aa5e17d45c129710a2cb50a9fbd
Frank Wiles <frank@revsys.com>
2020-11-10 16:05:56 -0600

    Fix tests as we now require the MODEL_TICKET

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,17 +1,16 @@
-DATABASES = {
-    'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': ':memory:'}
-}
-SECRET_KEY = 'fake-key'
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
+SECRET_KEY = "fake-key"
 INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
     "modelservice",
     "tests",
 ]
 
-ROOT_URLCONF = 'modelservice.urls'
+ROOT_URLCONF = "modelservice.urls"
 
-SIMPL_GAMES_URL = 'http://localhost:9000'
-SIMPL_GAMES_AUTH = ('test', 'test')
-ROOT_TOPIC = 'com.example'
+SIMPL_GAMES_URL = "http://localhost:9000"
+SIMPL_GAMES_AUTH = ("test", "test")
+ROOT_TOPIC = "com.example"
+MODEL_TICKET = "fake-ticket"
+
