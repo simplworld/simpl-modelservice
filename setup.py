df05cd458a1023cec74c8d0e1c33acb320ffbd49
Jane Eisenstein <ejane@softweave.com>
2020-12-09 12:56:52 -0500

    Bump version: 0.9.0 → 0.9.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-VERSION = '0.9.0'
+VERSION = '0.9.1'
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
     readme = f.read()
